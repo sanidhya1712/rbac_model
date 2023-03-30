58b9660a98693ba5985aa119c00b958614f5b11c
Sanidhya Daeeyya <Sanidhya.Daeeyya@truminds.com>
2023-03-30 15:52:21 +0530

    models

---
+++ models/roles.ts
@@ -0,0 +1,12 @@
+class roles {
+    role : string
+    constructor(role: string) {
+        this.role = role
+    }
+    printRole() {
+        console.log("role--", this.role)
+        return this.role
+    }
+}
+
+export default roles
